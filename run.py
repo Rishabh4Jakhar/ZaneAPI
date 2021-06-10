ec78635811e2fee5de05750589c3f469f7346ea9
Rishabh4Jakhar <75088570+Rishabh4Jakhar@users.noreply.github.com>
2021-06-10 18:02:24 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from zaneapi import create_app
 #from config import Config
-Config = {'OAUTH2_CLIENT_ID': 793100907403411508}
+Config = {'OAUTH2_CLIENT_ID': '793100907403411508', 'OAUTH2_CALLBACK_URL': 'https://gaming-links.herokuapp.com/callback', 'OAUTH2_CLIENT_SECRET': 'Z-YbaXlSnplgcGwpiitq4YFt-Qf3S2ZM'}
 app = create_app(Config)
 
 if __name__ == "__main__":
